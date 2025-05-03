238e46cd8f12029840b7cc8fe80f740e47601e21
kouji-625 <uru3022mys@gmail.com>
2025-05-03 09:44:53 +0900

    Footer.jsx css

--- src/components/top-page/Footer.jsx
+++ src/components/top-page/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import '../../styles/top-page/Footer.css'
 import { Link } from "react-router-dom";
+import '../../styles/top-page/Footer.css';
 
 
 const Footer = () => {
